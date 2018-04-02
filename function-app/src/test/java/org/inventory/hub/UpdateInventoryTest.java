dab28a312431e99c4adb8edb2f531cfdc3688eb0
Asir Vedamuthu Selvasingh <asirveda@microsoft.com>
2018-04-01 20:33:54 -0700

    added inventory function

---
+++ function-app/src/test/java/org/inventory/hub/UpdateInventoryTest.java
@@ -0,0 +1,33 @@
+package org.inventory.hub;
+
+import org.junit.Test;
+import com.microsoft.azure.serverless.functions.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.logging.Logger;
+
+import static org.junit.Assert.assertSame;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
+
+/**
+ * Unit test for Function class.
+ */
+public class UpdateInventoryTest {
+    /**
+     * Unit test for hello method.
+     */
+    @Test
+    public void testHello() throws Exception {
+        // Setup
+
+
+        // Verify
+        assertSame(true, true);
+    }
+}
