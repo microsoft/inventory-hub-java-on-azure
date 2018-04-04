f116736679111af7a68e375dfeb91c75a9fa1e2e
Asir Vedamuthu Selvasingh <asirveda@microsoft.com>
2018-04-04 14:02:25 -0700

    fixed post processing

--- function-app/post-process-function.json.sh
+++ function-app/post-process-function.json.sh
@@ -1,4 +1,4 @@
 sed -i -e 's/documentdb/cosmosDb/g' target/azure-functions/*/Update-Inventory/function.json
 sed -i -e 's/path/eventHubName/g' target/azure-functions/*/Point-of-Sale/function.json
 sed -i -e 's/path/eventHubName/g' target/azure-functions/*/Update-Inventory/function.json
-sed -i -e 's/connection/connectionStringSetting/1' target/azure-functions/*/Update-Inventory/function.json
+sed -i -e '17s/connection/connectionStringSetting/' target/azure-functions/*/Update-Inventory/function.json
