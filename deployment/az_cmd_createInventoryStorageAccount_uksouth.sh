6a865ca47b47ef9e1cb4a24f3b6deaf73be55cd0
milismsft-mac <adrianmi@microsoft.com>
2018-04-17 00:34:30 -0700

    updates and fixes

---
+++ deployment/az_cmd_createInventoryStorageAccount_uksouth.sh
@@ -0,0 +1,3 @@
+az storage account create --resource-group "inventory-hub-uksouth-dev" --name "inventorysa4eventhubdev2"
+az storage container create --account-name "inventorysa4eventhubdev2" --name "notifications-eventhub"
+
