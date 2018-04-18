5ddad86fbf61b928ae1cd1e023bf8fa1ced77685
milismsft-mac <adrianmi@microsoft.com>
2018-04-18 13:49:15 -0700

    dashboard related changes

--- dashboard-web-app/src/main/resources/static/app.js
+++ dashboard-web-app/src/main/resources/static/app.js
@@ -34,21 +34,18 @@ function disconnect() {
     console.log("Disconnected");
 }
 
-function sendName() {
-    stompClient.send("/app/message", {}, JSON.stringify({'name': $("#name").val()}));
-}
-
 function showGreeting(message) {
     console.log("Received = " + message);
     // $("#greetings").append("<tr><td>" + message + "</td></tr>");
     $("#greetings").prepend("<tr><td>" + message + "</td></tr>");
+
 }
 
 $(function () {
+    connect();
     $("form").on('submit', function (e) {
         e.preventDefault();
     });
     $( "#connect" ).click(function() { connect(); });
     $( "#disconnect" ).click(function() { disconnect(); });
-    $( "#send" ).click(function() { sendName(); });
 });
