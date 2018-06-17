87520bb1185c7688a3825be0ca3da255e2b406c1
Asir Vedamuthu Selvasingh <asirveda@microsoft.com>
2018-06-16 19:08:46 -0700

    enabled kafka to event hub

--- dashboard-web-app/src/main/java/org/inventory/hub/InventoryHub.java
+++ dashboard-web-app/src/main/java/org/inventory/hub/InventoryHub.java
@@ -10,8 +10,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+
+import org.codehaus.jackson.map.ObjectMapper;
+
+import org.inventory.hub.controller.TransactionsController;
+import org.inventory.hub.event.Transaction;
+
+@EnableBinding(Sink.class)
 @SpringBootApplication
 public class InventoryHub extends SpringBootServletInitializer {
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    Transaction transaction = new Transaction();
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
@@ -21,6 +35,34 @@ public class InventoryHub extends SpringBootServletInitializer {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(InventoryHub.class, args);
     }
+
+    @StreamListener(target=Sink.INPUT)
+	public void receiveTransaction(String transactionMessage) {
+
+        System.out.println("message is: " + transactionMessage);
+
+        try 
+        {
+            transaction = objectMapper.readValue(
+                new String(transactionMessage.getBytes(), "UTF8"), Transaction.class);
+
+            TransactionsController.transactions.addFirst(transaction);
+
+            System.out.println("=== event data ===\n" + transaction.toString());
+        
+        } catch (Exception e)
+        {
+            System.out.println("Processing failed for an event: " + e.toString());
+			e.printStackTrace();
+        }
+
+    }
+    
+    @StreamListener("errorChannel")
+	public void errorGlobal(Message<?> message) {
+		System.out.println("Handling ERROR: " + message);
+	}
+
 
 }
 
