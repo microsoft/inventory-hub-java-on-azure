fff84997ae3288d5b32bc744aaa20cb86acd3754
milismsft-mac <adrianmi@microsoft.com>
2018-07-30 08:50:18 -0700

    Various fixes

--- deployment/az_cmd_createInventoryEventHubs_southeastasia.sh
+++ deployment/az_cmd_createInventoryEventHubs_southeastasia.sh
@@ -1,10 +1,11 @@
 # EventHub in "southeastasia"
 az group create --name "inventory-hub-southeastasia-dev" --location "southeastasia"
-az group deployment create --name "inventory-hub-eventhub-southeastasia-td1" --resource-group "inventory-hub-southeastasia-dev" --template-file createInventoryEventHub.json --parameters namespace_name="inventory-eventhub-southeastasia-dev1" region_name="southeastasia"
+az group deployment create --name "inventory-hub-eventhub-southeastasia-td1" --resource-group "inventory-hub-southeastasia-dev" --template-file createInventoryEventHub.json --parameters namespace_name="inventory-eventhub-southeastasia-kafka1" region_name="southeastasia"
 
 # list authorization rule keys and connection strings
-az eventhubs eventhub authorization-rule keys list --resource-group "inventory-hub-southeastasia-dev" --namespace-name "inventory-eventhub-southeastasia-dev1" --eventhub-name "eventhub-for-notifications" --name "intakeEvents"
-az eventhubs eventhub authorization-rule keys list --resource-group "inventory-hub-southeastasia-dev" --namespace-name "inventory-eventhub-southeastasia-dev1" --eventhub-name "eventhub-for-notifications" --name "dispatchEvents"
-az eventhubs eventhub authorization-rule keys list --resource-group "inventory-hub-southeastasia-dev" --namespace-name "inventory-eventhub-southeastasia-dev1" --eventhub-name "eventhub-for-transactions" --name "intakeEvents"
-az eventhubs eventhub authorization-rule keys list --resource-group "inventory-hub-southeastasia-dev" --namespace-name "inventory-eventhub-southeastasia-dev1" --eventhub-name "eventhub-for-transactions" --name "dispatchEvents"
+az eventhubs eventhub authorization-rule keys list --resource-group "inventory-hub-southeastasia-dev" --namespace-name "inventory-eventhub-southeastasia-kafka1" --eventhub-name "eventhub-for-notifications" --name "intakeEvents"
+az eventhubs eventhub authorization-rule keys list --resource-group "inventory-hub-southeastasia-dev" --namespace-name "inventory-eventhub-southeastasia-kafka1" --eventhub-name "eventhub-for-notifications" --name "dispatchEvents"
+az eventhubs eventhub authorization-rule keys list --resource-group "inventory-hub-southeastasia-dev" --namespace-name "inventory-eventhub-southeastasia-kafka1" --eventhub-name "eventhub-for-notifications" --name "myerrors"
+az eventhubs eventhub authorization-rule keys list --resource-group "inventory-hub-southeastasia-dev" --namespace-name "inventory-eventhub-southeastasia-kafka1" --eventhub-name "eventhub-for-transactions" --name "intakeEvents"
+az eventhubs eventhub authorization-rule keys list --resource-group "inventory-hub-southeastasia-dev" --namespace-name "inventory-eventhub-southeastasia-kafka1" --eventhub-name "eventhub-for-transactions" --name "dispatchEvents"
 
