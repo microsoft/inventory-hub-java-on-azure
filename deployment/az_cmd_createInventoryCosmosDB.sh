13e49a1633a76d526d8867ff82e5953fae8700b0
milismsft-mac <adrianmi@microsoft.com>
2018-08-01 15:03:16 -0700

    update scripts with parameters

--- deployment/az_cmd_createInventoryCosmosDB.sh
+++ deployment/az_cmd_createInventoryCosmosDB.sh
@@ -1,16 +1,21 @@
+if [ "$INVENTORY_HUB_APP_NAME" == "" ]; then echo "Unexpected error: environment variable INVENTORY_HUB_APP_NAME is not defined. Exiting..." && exit 1 
+fi
+
+echo "Using ""$INVENTORY_HUB_APP_NAME"" as a prefix for the names of the resources to be created"
+
 # CosmosDB account
-az group create --name "inventory-hub-main-dev" --location "eastus"
-az group deployment create --name "inventory-hub-cosmosdb-td1" --resource-group "inventory-hub-main-dev" --template-file ../deployment/createInventoryCosmosDB.json --parameters cosmosDBName="inventory-hub-cosmosdb-main-dev1" primaryLocation="eastus" secondaryLocation="uksouth" thirdLocation="southeastasia" 
+az group create --name "$INVENTORY_HUB_APP_NAME""-main-dev" --location "eastus"
+az group deployment create --name "$INVENTORY_HUB_APP_NAME""-cosmosdb-td1" --resource-group "$INVENTORY_HUB_APP_NAME""-main-dev" --template-file ../deployment/createInventoryCosmosDB.json --parameters cosmosDBName="$INVENTORY_HUB_APP_NAME""-cosmosdb-main-dev1" primaryLocation="eastus" secondaryLocation="uksouth" thirdLocation="southeastasia" 
 
 # CosmosDB Database
-az cosmosdb database create --resource-group "inventory-hub-main-dev" --name "inventory-hub-cosmosdb-main-dev1" --db-name "inventory"
+az cosmosdb database create --resource-group "$INVENTORY_HUB_APP_NAME""-main-dev" --name "$INVENTORY_HUB_APP_NAME""-cosmosdb-main-dev1" --db-name "inventory"
 
 # CosmosDB collections
-az cosmosdb collection create --resource-group "inventory-hub-main-dev" --name "inventory-hub-cosmosdb-main-dev1" --db-name "inventory" --collection-name "product-items" --partition-key-path "/productId" --throughput 5000
-az cosmosdb collection create --resource-group "inventory-hub-main-dev" --name "inventory-hub-cosmosdb-main-dev1" --db-name "inventory" --collection-name "product-inventory" --partition-key-path "/location" --throughput 2000
-az cosmosdb collection create --resource-group "inventory-hub-main-dev" --name "inventory-hub-cosmosdb-main-dev1" --db-name "inventory" --collection-name "transaction-events" --partition-key-path "/type" --throughput 2000
+az cosmosdb collection create --resource-group "$INVENTORY_HUB_APP_NAME""-main-dev" --name "$INVENTORY_HUB_APP_NAME""-cosmosdb-main-dev1" --db-name "inventory" --collection-name "product-items" --partition-key-path "/productId" --throughput 5000
+az cosmosdb collection create --resource-group "$INVENTORY_HUB_APP_NAME""-main-dev" --name "$INVENTORY_HUB_APP_NAME""-cosmosdb-main-dev1" --db-name "inventory" --collection-name "product-inventory" --partition-key-path "/location" --throughput 2000
+az cosmosdb collection create --resource-group "$INVENTORY_HUB_APP_NAME""-main-dev" --name "$INVENTORY_HUB_APP_NAME""-cosmosdb-main-dev1" --db-name "inventory" --collection-name "transaction-events" --partition-key-path "/type" --throughput 2000
 
 # Add another region "westus2"
-# az cosmosdb update --resource-group "inventory-hub-main-dev" --name "inventory-hub-cosmosdb-main-dev1" --locations "eastus"=0 "uksouth"=1 "southeastasia"=2 "westus2"=3
+# az cosmosdb update --resource-group "$INVENTORY_HUB_APP_NAME""-main-dev" --name "$INVENTORY_HUB_APP_NAME""-cosmosdb-main-dev1" --locations "eastus"=0 "uksouth"=1 "southeastasia"=2 "westus2"=3
 
 
--- deployment/az_cmd_createInventoryEventHubs_eastus_uksouth.sh
+++ deployment/az_cmd_createInventoryEventHubs_eastus_uksouth.sh
@@ -1,19 +1,24 @@
+if [ "$INVENTORY_HUB_APP_NAME" == "" ]; then echo "Unexpected error: environment variable INVENTORY_HUB_APP_NAME is not defined. Exiting..." && exit 1 
+fi
+
+echo "Using ""$INVENTORY_HUB_APP_NAME"" as a prefix for the names of the resources to be created"
+
 # EventHub in "eastus"
-az group create --name "inventory-hub-eastus-dev" --location "eastus"
-az group deployment create --name "inventory-hub-eventhub-eastus-td1" --resource-group "inventory-hub-eastus-dev" --template-file createInventoryEventHub.json --parameters namespace_name="inventory-eventhub-eastus-dev1" region_name="eastus"
+az group create --name "$INVENTORY_HUB_APP_NAME""-eastus-dev" --location "eastus"
+az group deployment create --name "$INVENTORY_HUB_APP_NAME""-eventhub-eastus-td1" --resource-group "$INVENTORY_HUB_APP_NAME""-eastus-dev" --template-file createInventoryEventHub.json --parameters namespace_name="$INVENTORY_HUB_APP_NAME""-eventhub-eastus" region_name="eastus"
 
 # List authorization rule keys and connection strings
-az eventhubs eventhub authorization-rule keys list --resource-group "inventory-hub-eastus-dev" --namespace-name "inventory-eventhub-eastus-dev1" --eventhub-name "eventhub-for-notifications" --name "intakeEvents"
-az eventhubs eventhub authorization-rule keys list --resource-group "inventory-hub-eastus-dev" --namespace-name "inventory-eventhub-eastus-dev1" --eventhub-name "eventhub-for-notifications" --name "dispatchEvents"
-az eventhubs eventhub authorization-rule keys list --resource-group "inventory-hub-eastus-dev" --namespace-name "inventory-eventhub-eastus-dev1" --eventhub-name "eventhub-for-transactionss" --name "intakeEvents"
-az eventhubs eventhub authorization-rule keys list --resource-group "inventory-hub-eastus-dev" --namespace-name "inventory-eventhub-eastus-dev1" --eventhub-name "eventhub-for-transactions" --name "dispatchEvents"
+az eventhubs eventhub authorization-rule keys list --resource-group "$INVENTORY_HUB_APP_NAME""-eastus-dev" --namespace-name "$INVENTORY_HUB_APP_NAME""-eventhub-eastus" --eventhub-name "eventhub-for-notifications" --name "intakeEvents"
+az eventhubs eventhub authorization-rule keys list --resource-group "$INVENTORY_HUB_APP_NAME""-eastus-dev" --namespace-name "$INVENTORY_HUB_APP_NAME""-eventhub-eastus" --eventhub-name "eventhub-for-notifications" --name "dispatchEvents"
+az eventhubs eventhub authorization-rule keys list --resource-group "$INVENTORY_HUB_APP_NAME""-eastus-dev" --namespace-name "$INVENTORY_HUB_APP_NAME""-eventhub-eastus" --eventhub-name "eventhub-for-transactions" --name "intakeEvents"
+az eventhubs eventhub authorization-rule keys list --resource-group "$INVENTORY_HUB_APP_NAME""-eastus-dev" --namespace-name "$INVENTORY_HUB_APP_NAME""-eventhub-eastus" --eventhub-name "eventhub-for-transactions" --name "dispatchEvents"
 
 # EventHub in "uksouth"
-az group create --name "inventory-hub-uksouth-dev" --location "uksouth"
-az group deployment create --name "inventory-hub-eventhub-uksouth-td1" --resource-group "inventory-hub-uksouth-dev" --template-file createInventoryEventHub.json --parameters namespace_name="inventory-eventhub-uksouth-dev1" region_name="uksouth"
+az group create --name "$INVENTORY_HUB_APP_NAME""-uksouth-dev" --location "uksouth"
+az group deployment create --name "$INVENTORY_HUB_APP_NAME""-eventhub-uksouth-td1" --resource-group "$INVENTORY_HUB_APP_NAME""-uksouth-dev" --template-file createInventoryEventHub.json --parameters namespace_name="$INVENTORY_HUB_APP_NAME""-eventhub-uksouth" region_name="uksouth"
 
 # List authorization rule keys and connection strings
-az eventhubs eventhub authorization-rule keys list --resource-group "inventory-hub-uksouth-dev" --namespace-name "inventory-eventhub-uksouth-dev1" --eventhub-name "eventhub-for-notifications" --name "intakeEvents"
-az eventhubs eventhub authorization-rule keys list --resource-group "inventory-hub-uksouth-dev" --namespace-name "inventory-eventhub-uksouth-dev1" --eventhub-name "eventhub-for-notifications" --name "dispatchEvents"
-az eventhubs eventhub authorization-rule keys list --resource-group "inventory-hub-uksouth-dev" --namespace-name "inventory-eventhub-uksouth-dev1" --eventhub-name "eventhub-for-transactions" --name "intakeEvents"
-az eventhubs eventhub authorization-rule keys list --resource-group "inventory-hub-uksouth-dev" --namespace-name "inventory-eventhub-uksouth-dev1" --eventhub-name "eventhub-for-transactions" --name "dispatchEvents"
+az eventhubs eventhub authorization-rule keys list --resource-group "$INVENTORY_HUB_APP_NAME""-uksouth-dev" --namespace-name "$INVENTORY_HUB_APP_NAME""-eventhub-uksouth" --eventhub-name "eventhub-for-notifications" --name "intakeEvents"
+az eventhubs eventhub authorization-rule keys list --resource-group "$INVENTORY_HUB_APP_NAME""-uksouth-dev" --namespace-name "$INVENTORY_HUB_APP_NAME""-eventhub-uksouth" --eventhub-name "eventhub-for-notifications" --name "dispatchEvents"
+az eventhubs eventhub authorization-rule keys list --resource-group "$INVENTORY_HUB_APP_NAME""-uksouth-dev" --namespace-name "$INVENTORY_HUB_APP_NAME""-eventhub-uksouth" --eventhub-name "eventhub-for-transactions" --name "intakeEvents"
+az eventhubs eventhub authorization-rule keys list --resource-group "$INVENTORY_HUB_APP_NAME""-uksouth-dev" --namespace-name "$INVENTORY_HUB_APP_NAME""-eventhub-uksouth" --eventhub-name "eventhub-for-transactions" --name "dispatchEvents"
