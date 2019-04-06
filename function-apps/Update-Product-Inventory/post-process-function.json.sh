597ba7063d2d690576f9cba3c779c2bb6066bbde
milismsft-mac <adrianmi@microsoft.com>
2019-04-06 16:26:26 -0700

    Update UPI function.

--- function-apps/Update-Product-Inventory/post-process-function.json.sh
+++ function-apps/Update-Product-Inventory/post-process-function.json.sh
@@ -1,5 +1,5 @@
 # Script to be run post build to set the content for running the function locally using the environment variable settings
 sed -i -e "s@UPI_FUNCTION_APP_STORAGE_ACCOUNT_CONNECTION_STRING@$UPI_FUNCTION_APP_STORAGE_ACCOUNT_CONNECTION_STRING@g" target/azure-functions/*/local.settings.json
 sed -i -e "s@TRANSACTIONS_EVENT_HUB_CONNECTION_STRING@$TRANSACTIONS_EVENT_HUB_CONNECTION_STRING@g" target/azure-functions/*/local.settings.json
-sed -i -e "s@PRODUCT_INVENTORY_DOCUMENTDB_CONNECTION_STRING@$PRODUCT_INVENTORY_DOCUMENTDB_CONNECTION_STRING@g" target/azure-functions/*/local.settings.json
+sed -i -e "s@PRODUCT_INVENTORY_COSMOSDB_CONNECTION_STRING@$PRODUCT_INVENTORY_COSMOSDB_CONNECTION_STRING@g" target/azure-functions/*/local.settings.json
 
