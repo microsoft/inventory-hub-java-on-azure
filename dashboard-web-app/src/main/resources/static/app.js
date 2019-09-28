f63c996c1258180275a807e12955c7d486e21dd5
Milis <adrianmi@microsoft.com>
2019-09-28 14:08:03 -0700

    Dashboard refactoring (#58)
    * testin 2 panels and removing refresh
    
    * updates
    
    * refactoring Web app client side.

--- dashboard-web-app/src/main/resources/static/app.js
+++ dashboard-web-app/src/main/resources/static/app.js
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+
 var stompClient = null;
 
 function setConnected(connected) {
@@ -18,10 +24,11 @@ function connect() {
     stompClient.connect({}, function (frame) {
         setConnected(true);
         console.log('Connected: ' + frame);
-        stompClient.subscribe('/topic/reply', function (greeting) {
+        stompClient.subscribe('/topic/reply', async function (greeting) {
             console.log("Received = " + greeting);
             console.log("===========");
             showGreeting(JSON.parse(greeting.body).name);
+            await showTable();
         });
     });
 }
@@ -39,6 +46,40 @@ function showGreeting(message) {
     // $("#greetings").append("<tr><td>" + message + "</td></tr>");
     $("#greetings").prepend("<tr><td>" + message + "</td></tr>");
 
+}
+
+async function showTable() {
+    console.log("Refreshing Product table content");
+
+    var response = await window.fetch('api/locations', {
+        method:"GET"
+    });
+
+    var locations = await response.json();
+
+    var headTable = '<tr><th rowspan="0"></th>';
+    Object.keys(locations).sort().forEach(function(location) {
+        headTable += '<th>' + location + '</th>';
+    });
+    headTable += "</tr>";
+
+    var response2 = await window.fetch('api/products', {
+        method:"GET"
+    });
+
+    var products = await response2.json();
+
+    Object.keys(products).forEach(function(productItem) {
+        headTable += '<tr>';
+        var product = products[productItem];
+        headTable += '<td>' + product['description'] + '</td>';
+        Object.keys(product.countByLocation).sort().forEach(function(countPerLocation) {
+            headTable += '<td>' + product.countByLocation[countPerLocation] + '</td>';
+        });
+        headTable += '</tr>';
+    });
+
+    $('#product-table').html(headTable);
 }
 
 $(function () {
